d00f50af196dd7063c5eebb19c6a20d0fa3a8a07
Zylern <47510180+Zylern@users.noreply.github.com>
2021-11-10 13:57:06 +0530

    Add files via upload

--- tortoolkit/functions/Leech_Module.py
+++ tortoolkit/functions/Leech_Module.py
@@ -539,17 +539,17 @@ async def errored_message(e, reason):
 
 
 async def print_files(e, files, thash=None, path=None, size=None):
-    msg = f"<a href='tg://user?id={e.sender_id}'>Done</a>\n#uploads\n"
+    msg = f"<a href='tg://user?id={e.sender_id}'>Your Requested Files:</a>    #uploads\n"
 
     if path is not None and size is None:
         size = calculate_size(path)
         transfer[0] += size
         size = human_readable_bytes(size)
-        msg += f"**Uploaded:** {str(size)}\n\n"
+        msg += f"**Total Size:** {str(size)}\n\n"
     elif size is not None:
         transfer[0] += size
         size = human_readable_bytes(size)
-        msg += f"**Uploaded:** {str(size)}\n\n"
+        msg += f"**Total Size:** {str(size)}\n\n"
 
     if len(files) == 0:
         return
