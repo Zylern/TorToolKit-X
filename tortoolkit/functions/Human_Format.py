11432ce659d5ebfddc3d802962acd37067f264f4
Zylern <47510180+Zylern@users.noreply.github.com>
2021-11-10 11:49:10 +0530

    Add files via upload

--- tortoolkit/functions/Human_Format.py
+++ tortoolkit/functions/Human_Format.py
@@ -25,22 +25,22 @@ def human_readable_timedelta(seconds, precision=0):
     value = timedelta(seconds=seconds)
 
     if value.days:
-        pieces.append(f"{value.days}d")
+        pieces.append(f"{value.days} D")
 
     seconds = value.seconds
 
     if seconds >= 3600:
         hours = int(seconds / 3600)
-        pieces.append(f"{hours}h")
+        pieces.append(f"{hours} H")
         seconds -= hours * 3600
 
     if seconds >= 60:
         minutes = int(seconds / 60)
-        pieces.append(f"{minutes}m")
+        pieces.append(f"{minutes} M")
         seconds -= minutes * 60
 
     if seconds > 0 or not pieces:
-        pieces.append(f"{seconds}s")
+        pieces.append(f"{seconds} S")
 
     if not precision:
         return "".join(pieces)
